dea4ba207f5ac7a00a0ccd1c6cdc8ead59f2a3f9
Felicia <ffeliciachang@gmail.com>
2022-04-02 11:18:08 -0700

    wip

--- src/App.js
+++ src/App.js
@@ -1,75 +1,89 @@
-import './App.css';
-import React from 'react';
+import "./App.css";
+import React from "react";
 
-const TextExpander = ({title, subtitle, link}) => {
+const TextExpander = ({ title, subtitle, link }) => {
   const [isActive, setIsActive] = React.useState(false);
 
   return (
     <span>
-      <span className="title" onClick={() => setIsActive(!isActive)}>{title} · </span>
-      {isActive && (<span className="subtitle">{`${subtitle} ${link}  · `}</span>)}
+      <span className="title" onClick={() => setIsActive(!isActive)}>
+        {title} {isActive ? " : " : " · "}{" "}
+      </span>
+      {isActive && (
+        <span onClick={() => window.open(link)}>
+          <span className="subtitle">{`${subtitle} ${
+            isActive ? "  " : " · "
+          }`}</span>
+          <span>{isActive ? " · " : "  "}</span>
+        </span>
+      )}
     </span>
-  )
-}
+  );
+};
 
 function App() {
   return (
     <div className="App">
       <div className="bio">
-          writing words, while loops and wandering wectors...
+        writing words, while loops and wandering wectors...
+      </div>
+      <TextExpander
+        title="Two Pages Double Spaced"
+        subtitle="an experiment in feeling too much"
+        link="https://twopagesdoublespaced.substack.com/"
+      />
+      <TextExpander
+        title="Love in the form of Taiwanese dinner"
+        subtitle="an essay on grief and family and the power of a good dinner"
+        link="https://audacity.substack.com/p/love-in-the-form-of-taiwanese-dinner?s=r"
+      />
+      <TextExpander
+        title="Neeva"
+        subtitle="reimagining search on the product innovation team"
+        link="https://neeva.com/about"
+      />
+      <TextExpander
+        title="Read Alibi"
+        subtitle="a website celebrati interactive poetry"
+        link="https://readalibi.org/invitation"
+      />
+      <TextExpander
+        title="Only Observations"
+        subtitle="senior thesis in computing in the arts challenging the ethics and politics of making"
+        link="https://docs.google.com/presentation/d/1udt4abrDLhs7Gn5PICJdox2cRSzfU4_5ClgvWRL238U/edit?usp=sharing"
+      />
+      <TextExpander
+        title="In Graphical Translation"
+        subtitle="senior thesis in computing in the arts playing with image as text"
+        link=""
+      />
+      <TextExpander
+        title="Lux et Veritas"
+        subtitle="a typographic installation at the becton cafe at yale"
+        link="https://www.youtube.com/watch?v=C1u5e_T_vbo"
+      />
+      <TextExpander
+        title="Dance Exercises"
+        subtitle="a practice in responding to sound with shape and color"
+        link="https://youtube.com/playlist?list=PLdso2CHmTwiyyGUasf1pLgsYokBWSkOkr"
+      />
+      <TextExpander
+        title="The Yale Politic"
+        subtitle="giving 75 years of stories a new outfit"
+        link="https://thepolitic.org/"
+      />
+      <TextExpander
+        title="Alone in Beijing"
+        subtitle="a collision of one woman's poem and another woman's daily commute"
+        link="https://www.youtube.com/watch?v=UFUqwYtCa44"
+      />
+      <div className="bio">
+        <div className="socials">
+          <a href="https://twitter.com/felchang">twitter</a>
+          <a href="https://twopagesdoublespaced.substack.com/">substack</a>
+          <a href="https://www.instagram.com/felsdelivery/">instagram</a>
         </div>
-        <TextExpander 
-          title="Two Pages Double Spaced" 
-          subtitle="an experiment in feeling too much"
-          link=""
-        />
-        <TextExpander 
-          title="Love in the form of Taiwanese dinner" 
-          subtitle="an essay on grief and family and the power of a good dinner"
-          link=""
-        />
-        <TextExpander 
-          title="Read Alibi" 
-          subtitle="a website celebrati interactive poetry"
-          link=""
-        />
-        <TextExpander 
-          title="Only Observations" 
-          subtitle="senior thesis in computing in the arts challenging the ethics and politics of making"
-          link=""
-        />
-        <TextExpander 
-          title="In Graphical Translation" 
-          subtitle="senior thesis in computing in the arts playing with image as text"
-          link=""
-        />
-        <TextExpander 
-          title="Lux et Veritas" 
-          subtitle="a typographic installation at the becton cafe at yale"
-          link=""
-        />
-        <TextExpander 
-          title="Dance Exercises" 
-          subtitle="a practice in responding to sound with shape and color"
-          link=""
-        />
-        <TextExpander 
-          title="The Yale Politic" 
-          subtitle="giving 75 years of stories a new outfit"
-          link=""
-        />
-        <TextExpander 
-          title="Alone in Beijing" 
-          subtitle="a collision of one woman's poem and another woman's daily commute"
-          link=""
-        />
-        <div className="bio">
-          <div className="socials">
-            <div>twitter</div>
-            <div>email</div>
-            <div>instagram</div>
-            </div>
-        </div>
+      </div>
     </div>
   );
 }
